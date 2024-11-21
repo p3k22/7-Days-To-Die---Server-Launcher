dba3edc966701648ad1b80ebebab9fbaa55ac7c3
P3k <paul.l.b@hotmail.co.uk>
2024-11-21 20:19:06 +0000

    Initial Commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _7_Days_To_Die___Server_Launcher
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
